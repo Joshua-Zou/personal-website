09150ed7989222208bd92383e0e9faf3e030fe81
Joshua-Zou <joshyzou@gmail.com>
2021-07-07 12:14:47 -0700

    added animations and new section

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ async function showMobileNavbar() {
     document.getElementsByClassName("right-navbar")[0].style.left = "0px";
 }
 function hideMobileNavbar() {
-    if (window.screen.width > 500) return;
     document.getElementsByClassName("right-navbar")[0].style.left = "-500px";
     document.getElementsByClassName("top-bar")[0].style.top = "0";
 }
@@ -26,3 +25,63 @@ function toggleMobileNavbar() {
         hideMobileNavbar();
     }
 }
+$(document).ready(function() {
+    var elements;
+    var windowHeight;
+    function init() {
+      elements = document.querySelectorAll('.scroll-from-left');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('left-scroll-element');
+          element.classList.remove('scroll-from-left');
+          if (element.classList.contains("intro"))addClassAfterTime(1000, "short-float", element)
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition(); 
+});
+
+async function addClassAfterTime(time, className, element){
+    await sleep(time)
+    element.classList.add(className)
+}
+
+$(document).ready(function() {
+   
+    var elements;
+    var windowHeight;
+    function init() {
+      elements = document.querySelectorAll('.scroll-from-right');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('right-scroll-element');
+          element.classList.remove('scroll-from-right');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition(); 
+});
