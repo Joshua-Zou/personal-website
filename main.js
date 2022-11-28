5789a0e28c43d5fb38b0eb2fef1b201bccfad57c
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-11-27 22:55:00 -0800

    Add experience section and update data

--- main.js
+++ main.js
@@ -1,89 +1,89 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 window.addEventListener('click', function (e) {
-    if (document.getElementsByClassName('mobile')[0].contains(e.target)) {
-        // Clicked in box
-    } else {
-        hideMobileNavbar();
-    }
+  if (document.getElementsByClassName('mobile')[0].contains(e.target)) {
+    // Clicked in box
+  } else {
+    hideMobileNavbar();
+  }
 });
 async function showMobileNavbar() {
-    document.getElementsByClassName("right-navbar")[0].style.left = "10px";
-    document.getElementsByClassName("top-bar")[0].style.top = "-61px";
-    await sleep(500)
-    document.getElementsByClassName("right-navbar")[0].style.left = "0px";
+  document.getElementsByClassName("right-navbar")[0].style.left = "10px";
+  document.getElementsByClassName("top-bar")[0].style.top = "-61px";
+  await sleep(500)
+  document.getElementsByClassName("right-navbar")[0].style.left = "0px";
 }
 function hideMobileNavbar() {
-    document.getElementsByClassName("right-navbar")[0].style.left = "-500px";
-    document.getElementsByClassName("top-bar")[0].style.top = "0";
+  document.getElementsByClassName("right-navbar")[0].style.left = "-500px";
+  document.getElementsByClassName("top-bar")[0].style.top = "0";
 }
 function toggleMobileNavbar() {
-    if (document.getElementsByClassName("right-navbar")[0].style.left !== "0px") {
-        showMobileNavbar();
-    } else {
-        hideMobileNavbar();
-    }
+  if (document.getElementsByClassName("right-navbar")[0].style.left !== "0px") {
+    showMobileNavbar();
+  } else {
+    hideMobileNavbar();
+  }
 }
-$(document).ready(function() {
-    var elements;
-    var windowHeight;
-    function init() {
-      elements = document.querySelectorAll('.scroll-from-left');
-      windowHeight = window.innerHeight;
-    }
-  
-    function checkPosition() {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
-  
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('left-scroll-element');
-          element.classList.remove('scroll-from-left');
-          if (element.classList.contains("intro"))addClassAfterTime(1000, "short-float", element)
-        }
+$(document).ready(function () {
+  var elements;
+  var windowHeight;
+  function init() {
+    elements = document.querySelectorAll('.scroll-from-left');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('left-scroll-element');
+        element.classList.remove('scroll-from-left');
+        if (element.classList.contains("intro")) addClassAfterTime(1000, "short-float", element)
       }
     }
-  
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  
-    init();
-    checkPosition(); 
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
 });
 
-async function addClassAfterTime(time, className, element){
-    await sleep(time)
-    element.classList.add(className)
+async function addClassAfterTime(time, className, element) {
+  await sleep(time)
+  element.classList.add(className)
 }
 
-$(document).ready(function() {
-   
-    var elements;
-    var windowHeight;
-    function init() {
-      elements = document.querySelectorAll('.scroll-from-right');
-      windowHeight = window.innerHeight;
-    }
-  
-    function checkPosition() {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
-  
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('right-scroll-element');
-          element.classList.remove('scroll-from-right');
-        }
+$(document).ready(function () {
+
+  var elements;
+  var windowHeight;
+  function init() {
+    elements = document.querySelectorAll('.scroll-from-right');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('right-scroll-element');
+        element.classList.remove('scroll-from-right');
       }
     }
-  
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  
-    init();
-    checkPosition(); 
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
 });
 
 function dragElement(elmnt) {
@@ -126,3 +126,55 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+$(document).ready(function () {
+  "use strict";
+  // Select all slides
+  const slides = document.querySelectorAll(".slide");
+
+  // loop through slides and set each slides translateX
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+  });
+
+  // select next slide button
+  const nextSlide = document.querySelector(".btn-next");
+
+  // current slide counter
+  let curSlide = 0;
+  // maximum number of slides
+  let maxSlide = slides.length - 1;
+
+  // add event listener and navigation functionality
+  nextSlide.addEventListener("click", function () {
+    // check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    //   move slide by -100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+  });
+
+  // select next slide button
+  const prevSlide = document.querySelector(".btn-prev");
+
+  // add event listener and navigation functionality
+  prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    } else {
+      curSlide--;
+    }
+
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+  });
+
+})
